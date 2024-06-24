216781d8361faf8d84cacf8cb733f8e78ef06cbb
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-24 19:31:18 +0200

    feat: page completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,11 @@
+const { createApp } = Vue
 
+createApp({
+    data() {
+        return {
+            message: 'Hello World!',
+            image: 'images/welcome-image.webp',
+            imageAlt: 'Hello!'
+        }
+    }
+}).mount('#app')
